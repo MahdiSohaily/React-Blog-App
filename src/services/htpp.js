157e81f13845c98b61f91eac582fd770e4b87a49
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-29 15:33:14 +0430

    Add http services for sending request

--- src/services/htpp.js
+++ src/services/htpp.js
@@ -1,3 +1,24 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
+class httpClient {
+  get(url, config = {}) {
+    return axios.get(url, config);
+  }
+
+  post(url, payload, config) {
+    return axios.post(url, payload, config);
+  }
+
+  put(url, payload, config) {
+    return axios.put(url, payload, config);
+  }
+
+  delete(url, config = {}) {
+    return axios.delete(url, config);
+  }
+}
+
+const http = new httpClient();
+
+export default http;

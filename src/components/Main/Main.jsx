50c42d83f14edcba6a2d928d9ab72611f55aac89
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-11-29 18:07:56 +0430

    Update HTML Structure

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,3 @@
 export default function Main({ children }) {
-    return (
-        <div className="main">
-            {children}
-        </div>
-    )
+  return <main className="main">{children}</main>;
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,21 +3,23 @@ import './style.css';
 
 export default function Nav() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <A href="/">Home Page</A>
-        </li>
-        <li>
-          <A href="/blog">Blog</A>
-        </li>
-        <li>
-          <A href="/contact">Contact Us</A>
-        </li>
-        <li>
-          <A href="/about">About Us</A>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <A href="/">Home Page</A>
+          </li>
+          <li>
+            <A href="/blog">Blog</A>
+          </li>
+          <li>
+            <A href="/contact">Contact Us</A>
+          </li>
+          <li>
+            <A href="/about">About Us</A>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
